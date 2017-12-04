aea8bfe33339eedea0e464038c7241de1ed31fba
Lee JaeBeom <btv9200@gmail.com>
2017-12-04 19:06:05 +0900

    kotlin study

---
+++ app/build/generated/source/buildConfig/production/debug/org/petabytes/awesomeblogs/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.petabytes.awesomeblogs;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.petabytes.awesomeblogs.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "production";
+  public static final int VERSION_CODE = 19;
+  public static final String VERSION_NAME = "1.9";
+  // Fields from default config.
+  public static final String GIT_SHA = "041fdbf";
+  public static final long GIT_TIMESTAMP = 1511834680L;
+}
